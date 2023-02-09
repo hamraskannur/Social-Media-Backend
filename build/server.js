ae10302a5e242c72a39185497110f6d8b090d445
hamraskannur <kkhamras741@gmail.com>
2023-02-09 19:37:15 +0530

    build

--- build/server.js
+++ build/server.js
@@ -17,7 +17,7 @@ const dbConnect = require("./config/connects");
 app.use(express_1.default.json());
 app.use(cookieParser());
 app.use(CORS({
-    origin: ["https://www.locomate.smartworlds.shop"],
+    origin: [process.env.BASE_URL],
     methods: ["GET", "POST", "PUT", "DELETE", "HEAD"],
     credentials: true,
     exposedHeaders: ["Content-Length", "X-Foo", "X-Bar"],
