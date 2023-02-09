fbdf7195214a9c8b033097e3f097a53e3b7b2059
hamraskannur <kkhamras741@gmail.com>
2023-02-09 18:59:09 +0530

    build

--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,7 @@ app.use(cookieParser());
 
 app.use(
   CORS({
-    origin: ["*"],
+    origin: [process.env.BASE_URL],
     methods: ["GET", "POST", "PUT", "DELETE","HEAD"],
     credentials: true,
     exposedHeaders: ["Content-Length", "X-Foo", "X-Bar"],
