5ae2b120287c73f8b83449fe83ccb24b61be89dd
hamraskannur <kkhamras741@gmail.com>
2023-02-09 20:00:50 +0530

    cors issue correcting

--- build/server.js
+++ build/server.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 // );
 app.use(function (req, res, next) {
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://locomate.smartworlds.shop');
+    res.setHeader('Access-Control-Allow-Origin', 'https://www.locomate.smartworlds.shop');
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE, HEAD');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
