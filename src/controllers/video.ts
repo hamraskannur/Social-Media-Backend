5b080906709146c646114296058b96aedf119ac0
hamraskannur <kkhamras741@gmail.com>
2023-02-10 10:22:33 +0530

    cors error

--- src/controllers/video.ts
+++ src/controllers/video.ts
@@ -20,7 +20,7 @@ export const uploadVideo = async (req: Request, res: Response) => {
 export const getAllVideo = async (req: Request, res: Response) => {
   try {
     const AllPosts = await postCollection
-      .find({shorts:{$exists:true}})
+      .find({shorts:{$ne:null}})
       .populate("userId", { username: 1, name: 1, _id: 1, ProfileImg: 1,public:1,Followers:1 });
       console.log(AllPosts);
       
